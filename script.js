4707b8329160b7583a481878e081465fcf5a75d1
KisKisBarsik <sadisintick@gmail.com>
2025-06-03 18:30:53 +0200

    Pil opp på alle nettsider, media query til alle piler og små endringer i feil-søk nettside

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Arrow to the top of the page starts here
+
+// Viser pilen når du scroller ned
+window.addEventListener('scroll', function () {    
+
+    if (window.innerWidth > 716 && window.scrollY > 450) {
+
+        arrowTopButton.style.display = 'flex';
+
+    } else {
+
+        arrowTopButton.style.display = 'none';
+
+    }
+
+})
+
+
+// Scroller smooth til top av nettside når du klikker på button med pillen
+document.getElementById("arrowTopButton").addEventListener('click', function (e) {
+
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth'})
+
+})
+
+// Arrow to the top of the page ends here
+
+
 // Hamburger menu starts here
 
 var hamburger = document.querySelector(".hamburger")
